59c1379c4c1800107b3ae0a48773113edf1afc2b
ioamra <iom.monchau@gmail.com>
2024-04-26 10:50:58 +0200

    class Creature

--- src/models/creatures/Creature.java
+++ src/models/creatures/Creature.java
@@ -1,9 +1,205 @@
 package models.creatures;
 
 public abstract class Creature {
+	private enum Gender { M, F };
+	private enum State {DEAD, SICK, HEALTHY};
+	
+	private String name;
+	private Gender gender;
+	private double weight;
+	private double size;
+	private final int MAX_AGE;
+	private int age;
+	private final int MAX_HUNGER;
+	private int hunger;
+	private boolean sleep;
+	private final int MAX_STAMINA;
+	private int stamina;
+	private State state;
+	private final int MAX_HP;
+	private int hp;
+	private final int ADD_PRICE;
+	private final int SELL_PRICE;
+	private final int MAINTENANCE_PRICE;
+	private final int MONEY_GAIN;
+	
+	// Constructor
+	public Creature(
+		String name, 
+		Gender gender, 
+		double weight, 
+		double size, 
+		int MAX_AGE, 
+		int MAX_HUNGER, 
+		int MAX_STAMINA, 
+		int MAX_HP, 
+		int ADD_PRICE, 
+		int SELL_PRICE, 
+		int MAINTENANCE_PRICE, 
+		int MONEY_GAIN
+	) {
+		this.setName(name);
+		this.setGender(gender);
+		this.setWeight(weight);
+		this.setSize(size);
+		this.MAX_AGE = MAX_AGE;
+		this.MAX_HUNGER = MAX_HUNGER;
+		this.MAX_STAMINA = MAX_STAMINA;
+		this.MAX_HP = MAX_HP; 
+		this.ADD_PRICE = ADD_PRICE; 
+		this.SELL_PRICE = SELL_PRICE;
+		this.MAINTENANCE_PRICE = MAINTENANCE_PRICE;
+		this.MONEY_GAIN = MONEY_GAIN;
+		this.setAge(0);
+		this.setHunger(MAX_HUNGER);
+		this.setStamina(MAX_STAMINA);
+		this.setHp(MAX_HP);
+		this.setSleep(false);
+		this.setState(State.HEALTHY);
+	}
+	
+	// Public Method
+	public void eat() {
+		this.setHunger(this.MAX_HUNGER);
+	}
+	
+	public void makeSound() {
+		
+	}
+	
+	public void heal() {
+		this.setHp(this.MAX_HP);
+	}
+	
+	public void toogleSleep() {
+		this.setSleep(!this.sleep);
+	}
+	
+	public void die() {
+		this.setState(State.DEAD);
+	}
+	
+	public void mate(Creature mate) {
+		
+	}
+	
+	public void checkAge() {
+		if (this.age > this.MAX_AGE) {
+			this.die();
+		}
+	}
 
-	public Creature() {
-		// TODO Auto-generated constructor stub
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getHunger() {
+		return hunger;
+	}
+
+	public void setHunger(int hunger) {
+		this.hunger = hunger;
+	}
+
+	public boolean isSleep() {
+		return sleep;
+	}
+
+	public void setSleep(boolean sleep) {
+		this.sleep = sleep;
+	}
+
+	public int getStamina() {
+		return stamina;
+	}
+
+	public void setStamina(int stamina) {
+		this.stamina = stamina;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getMAX_AGE() {
+		return MAX_AGE;
+	}
+
+	public int getMAX_HUNGER() {
+		return MAX_HUNGER;
+	}
+
+	public int getMAX_STAMINA() {
+		return MAX_STAMINA;
+	}
+
+	public int getMAX_HP() {
+		return MAX_HP;
+	}
+
+	public int getADD_PRICE() {
+		return ADD_PRICE;
+	}
+
+	public int getSELL_PRICE() {
+		return SELL_PRICE;
+	}
+
+	public int getMAINTENANCE_PRICE() {
+		return MAINTENANCE_PRICE;
+	}
+
+	public int getMONEY_GAIN() {
+		return MONEY_GAIN;
 	}
 
 }
