73a27a97c7958b2b71ec98434318df0f1af1d929
Jadorelethe <vlad.hayevskyy@gmail.com>
2024-05-30 20:43:21 +0200

    modal and controller

--- src/controllers/modals/BuyCreatureModalController.java
+++ src/controllers/modals/BuyCreatureModalController.java
@@ -1,5 +1,40 @@
 package controllers.modals;
 
-public class BuyCreatureModalController {
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
+
+public class BuyCreatureModalController 
+{
+	
+	
+	private TextField nameField;
+	private ChoiceBox<String> creatureTypeChoiceBox;
+	
+	
+	public void initialize() 
+	{
+		creatureTypeChoiceBox.getItems().addAll("Dragon", "Kraken", "Lycanthropes", "Megalodon", "Mermaid", "Nymph", "Phoenix", "Unicorn" );
+		creatureTypeChoiceBox.setValue(null);
+	}
+	
+	
+	@FXML
+	private void buyCreature() 
+	{
+		String name = nameField.getText();
+		String type = creatureTypeChoiceBox.getValue();
+		
+		processPurchase(name, type);
+	}
+	
+	
+	private void processPurchase(String name, String type) 
+	{
+		if (name != null && type != null)
+		{
+			System.out.println("Traitement de l'achat de la créature : " + type + " nommée" + name);
+		}
+	}
 }
--- src/controllers/modals/UpgradeEnclosureModalController.java
+++ src/controllers/modals/UpgradeEnclosureModalController.java
@@ -1,5 +1,84 @@
 package controllers.modals;
 
-public class UpgradeEnclosureModalController {
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class UpgradeEnclosureModalController 
+{
+
+	
+    @FXML
+    private ChoiceBox<String> enclosureTypeChoiceBox;
+
+    
+    @FXML
+    private Label depthLabel;
+
+    
+    @FXML
+    private TextField depthField;
+
+    
+    @FXML
+    private Label heightLabel;
+
+    
+    @FXML
+    private TextField heightField;
+
+    
+    @FXML
+    private TextField surfaceField;
+
+    
+    @FXML
+    public void initialize() 
+    {
+        enclosureTypeChoiceBox.getItems().addAll("Terrestre", "Aquarium", "Aviary");
+    }
+
+    
+    @FXML
+    private void handleEnclosureTypeSelection() 
+    {
+        String selectedType = enclosureTypeChoiceBox.getValue();
+        if (selectedType != null) 
+        {
+            switch (selectedType) 
+            {
+                case "Terrestre":
+                    surfaceField.setVisible(true);
+                    depthLabel.setVisible(false);
+                    depthField.setVisible(false);
+                    heightLabel.setVisible(false);
+                    heightField.setVisible(false);
+                    break;
+                case "Aquarium":
+                    surfaceField.setVisible(true);
+                    depthLabel.setVisible(true);
+                    depthField.setVisible(true);
+                    heightLabel.setVisible(false);
+                    heightField.setVisible(false);
+                    break;
+                case "Aviary":
+                    surfaceField.setVisible(true);
+                    depthLabel.setVisible(false);
+                    depthField.setVisible(false);
+                    heightLabel.setVisible(true);
+                    heightField.setVisible(true);
+                    break;
+            }
+        }
+    }
+
+    @FXML
+    private void upgradeSurface() 
+    {
+        String surface = surfaceField.getText();
+        System.out.println("Surface améliorée à : " + surface);
+    }
 
 }
